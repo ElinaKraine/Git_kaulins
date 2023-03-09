a5f1705c368878c51bba35ba8e8496a2a7a9cba7
Kraine <elina.kraine@sk.lvt.lv>
2023-03-09 12:56:32 +0200

    Nodrošināta iespēja ievadīt metienu reižu skaitu

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,12 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Cirks {
 
 	public static void main(String[] args) {
-		int skaitlis;
+		int skaitlis, reizes;
+		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		System.out.println("Cik reizes mest kauliņu? ");
+		reizes = scan.nextInt();
+		scan.close();
+		for(int i=1; i<=reizes; i++){
+			skaitlis = rand.nextInt(6)+1;
+			System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
 
 	}
 
