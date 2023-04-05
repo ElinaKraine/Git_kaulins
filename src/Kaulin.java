afc5340df243137bd86bb6bdd3cf3d42947f9fb3
Kraine <elina.kraine@sk.lvt.lv>
2023-04-05 09:57:27 +0300

    Papildīnats ar lielāko skaitla meklešanu

---
+++ src/Kaulin.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Kaulin {
+	static void mestKaulinu(int reizes) {
+		int skaitlis, lielakais=0;
+		Random rand = new Random();
+		for(int i=1; i<=reizes; i++) {
+			skaitlis = rand.nextInt(6)+1;	
+			System.out.println("Uzkrita skaitlis "+skaitlis);
+			if(lielakais<skaitlis)
+				lielakais=skaitlis;
+				
+		}
+		System.out.println("Lielākais skaitlis no metienem bija: "+lielakais);
+	}
+	
+	public static void main(String[] args) {
+		int reizes;
+		Scanner scan = new Scanner(System.in);
+		do {
+			System.out.println("Cik reizes mest kauliņu?");
+			reizes = scan.nextInt();
+		}while(reizes<1);
+		mestKaulinu(reizes);
+		scan.close();
+	}
+}
+
